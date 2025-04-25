5946ffd5ffdc403b846a80e87d01c98f2be507ba
baplaninarenje <157408243+baplaninarenje@users.noreply.github.com>
2025-04-25 13:41:48 +0200

    Add layout view template

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const path = require('node:path');
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts'); // Import express-ejs-layouts
 const app = express();
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Use express-ejs-layouts middleware
+app.use(expressLayouts); // This will automatically apply the layout to all views
+
+// Set the default layout (optional if you want a different layout filename)
+app.set('layout', 'layout'); // This tells express-ejs-layouts to use layout.ejs by default
+
 const indexRouter = require('./routes/indexRouter');
 const newMessageRouter = require('./routes/newMessageRouter');
 const messageRouter = require('./routes/messageRouter');
 
+// Set up routes
 app.use('/message', messageRouter);
 app.use('/new', newMessageRouter);
 app.use('/', indexRouter);
--- controllers/newMessageController.js
+++ controllers/newMessageController.js
@@ -2,7 +2,7 @@ const messagesModel = require('../models/messages');
 
 module.exports = {
   get: async (req, res) => {
-    res.render('form');
+    res.render('form', { title: 'Post a New Message' });
   },
   post: async (req, res) => {
     const { messageText, messageUser } = req.body;
